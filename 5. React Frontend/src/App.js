9e28894f3a4070ee5463f78c88e9de3061c436a1
abdjahiduddin <abd.jahiduddin@gmail.com>
2022-06-29 23:56:02 +0700

    Parent Child Component, Lists

---
+++ 5. React Frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ChartItem from './Lists/ChartItem';
+import EmployeeList from './Lists/EmployeeList';
+
+function App() {
+  return (
+    <div>
+      <ChartItem/>
+      <EmployeeList/>
+    </div>
+  );
+}
+
+export default App;
